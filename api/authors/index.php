9c900489bf48ff267d7f10a6e24b3a5ff79c7c56
Todd <woodtm@comcast.net>
2023-03-05 11:03:30 -0800

    correct headers

--- api/authors/index.php
+++ api/authors/index.php
@@ -4,7 +4,7 @@
      $method = $_SERVER['REQUEST_METHOD'];
  
      if ($method === 'OPTIONS') {
-         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+         header('Access-Control-Allow-Methods: DELETE');
          header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
          exit();
      } 
